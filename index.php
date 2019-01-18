9dd3ac7eee46bb35a56e4db695b9b6ebcedc56cf
capawing <35475195+capawing@users.noreply.github.com>
2019-01-18 15:45:35 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Use Case Diagram</title>
+</head>
+<body>
+<img src="Use Case Diagram">
+<p>This Use case diagram shows that the Actors (Passenger,Driver and Admin) associate with a multiple use cases.
+<br>Passenger use case related, he/she considering as a bus commuters,as a part of the application processes 
+<br>fare payments it's includes one of importants use case throught the bus commuters. the route guide is just 
+<br>extends optional only,when their is new commuters, that they realy don't know about the entire bus routes destinations,
+<br>Driver is our secondary user, why it's because his process it defends all about the passenger,if they don't passenger 
+<br>on that day, so no transaction happened.The Admin is mid-man of the entire processing applications ,it's because he/she is 
+<br>the one access and manage and input all the Daily sales transaction and that is part includes as Admin actor.</br></p>
+</body>
+</html>

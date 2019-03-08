53ef3d6ed914cf3a3b5ca67a1ff4de57843e6984
capawing <35475195+capawing@users.noreply.github.com>
2019-03-07 23:19:26 -0800

    Update index.php

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
 </head>
 <body>
 <img src="Use Case Diagram.jpg">
-<p>This Use case diagram shows that the Actors (Passenger,Driver and Admin) associate with a multiple use cases.
-<br>Passenger use case related, he/she considering as a bus commuters,as a part of the application processes 
-<br>fare payments it's includes one of importants use case throught the bus commuters. the route guide is just 
-<br>extends optional only,when their is new commuters, that they realy don't know about the entire bus routes destinations,
-<br>Driver is our secondary user, why it's because his process it defends all about the passenger,if they don't passenger 
+<p>This Use case diagram shows that the Actors (Riders,Driver and Admin) associate with a multiple use cases.
+<br>Riders use case related, he/she considering as a bus commuters,as a part of the application processes 
+<br>fare Guide it's includes one of importants use case for the bus commuters,these serve us your choices guide to select 
+<br>service do you prepared,select of what kind of riders you are and selection of buses fare.either it's ordinary bus or aircone.
+<br>Driver is our secondary user, why it's because his process it defends all about the passenger,if they don't have passenger 
 <br>on that day, so no transaction happened.The Admin is mid-man of the entire processing applications ,it's because he/she is 
 <br>the one access and manage and input all the Daily sales transaction and that is part includes as Admin actor.</br></p>
 </body>
